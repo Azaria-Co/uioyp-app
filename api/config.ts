06bfa0393b4f0bd4d9ebffddef7bbf5c55758087
Alejandro Macias <alexid.macias@gmail.com>
2025-09-05 12:51:23 -0600

    upsi

--- api/config.ts
+++ api/config.ts
@@ -1,4 +1,4 @@
 // api/config.ts
-export const API_URL = "http://10.10.100.74:3000"; // Cambiar por IP local o IP del servidor
+export const API_URL = "http://[IP_ADD]:3000"; // Cambiar por IP local o IP del servidor
 
 
